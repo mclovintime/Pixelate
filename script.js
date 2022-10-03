a83f8be0c9363a5a3c280c9207fed1b0dfb4d80f
Allyson Mitchem <allysonmitchem@gmail.com>
2022-10-03 08:53:00 -0600

    Allyson first

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Your code here
+const table= document.getElementsByTagName('table')[0]
+
+function makeRow (){
+    const row = document.createElement('tr')
+    for (let i = 0; i < 20; i++){
+        const td = document.createElement('td')
+        row.appendChild(td)
+    }
+    table.appendChild(row)
+}
+makeRow()
+makeRow()
+makeRow()
+
+const addRow = document.getElementById("add-row")
+addRow.addEventListener("click", function(){makeRow()})
+
+
+
